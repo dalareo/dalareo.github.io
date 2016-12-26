8e5809a3b36d0228c691a50590d5bd91665fb5a9
David A. Lareo <dalareo@gmail.com>
2016-12-26 11:19:53 +0100

    added sitemap

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+import fs from 'fs-extra-promise'
+import sm from 'sitemap'
+
 var rucksack = require('rucksack-css')
 var lost = require("lost")
 var cssnext = require("postcss-cssnext")
@@ -19,4 +22,36 @@ exports.modifyWebpackConfig = function(config, env) {
     })
 
     return config
+};
+
+function pagesToSitemap(pages) {
+  const urls = pages.map((p) => {
+    if (p.path !== undefined) {
+      return {
+        url: p.path,
+        changefreq: 'daily',
+        priority: 0.7
+      }
+    }
+  })
+  // remove undefined (template pages)
+  return urls.filter(u => u !== undefined)
+};
+
+function generateSiteMap(pages) {
+  const sitemap = sm.createSitemap({
+    hostname: 'https://dalareo.github.io',
+    cacheTime: '60000',
+    urls: pagesToSitemap(pages),
+  })
+  console.log('Generating sitemap.xml')
+  fs.writeFileSync(
+    `${__dirname}/public/sitemap.xml`,
+    sitemap.toString()
+  )
+};
+
+export function postBuild(pages, callback) {
+  generateSiteMap(pages)
+  callback()
 };
