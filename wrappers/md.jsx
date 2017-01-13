6d1642c9732f224bd3f1fa91b7b63d935a27918e
David A. Lareo <dalareo@gmail.com>
2017-01-13 12:18:26 +0100

    error with duplicated div

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -24,7 +24,6 @@ class MarkdownWrapper extends React.Component {
                 title={ `${post.title} - ${config.siteTitle}` }
               />
               { template }
-              </div>
             </div>
             );
     }
